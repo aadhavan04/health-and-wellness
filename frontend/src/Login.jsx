28e7631e8abfae915e8b672f7ae27fed917c540a
AADHAVAN R <aadhavan142@gmail.com>
2026-04-23 11:35:36 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -14,10 +14,10 @@ function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
-        data
-      );
+      const res = axios.post(
+  "https://health-and-wellness-1-cx6w.onrender.com/api/auth/login",
+  data
+);
 
       
       setError("");
